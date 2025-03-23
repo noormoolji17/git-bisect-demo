6345d582702782d1a92e4bb3dff16d83750ee24f
Michael <michael.lydeamore@monash.edu>
2025-03-23 04:00:52 +0000

    Add receipt formatting

---
+++ R/receipts.R
@@ -0,0 +1,20 @@
+format_money <- function(amount) {
+  sprintf("$%.2f", amount)
+}
+
+receipt_line <- function(item) {
+  paste(
+    item$size,
+    item$milk,
+    item$drink,
+    paste0("x", item$shots),
+    format_money(line_total(item$drink, item$size, item$milk, item$shots))
+  )
+}
+
+receipt <- function(items, student_discount = FALSE) {
+  c(
+    vapply(items, receipt_line, character(1)),
+    paste("Total", format_money(order_total(items, student_discount)))
+  )
+}
