2999ab06c836ca272cf4c916c6929d27476f516e
Rob <rob.hyndman@monash.edu>
2025-03-23 07:31:18 +0000

    Add morning rush example

---
+++ examples/morning-rush.R
@@ -0,0 +1,9 @@
+source("coffee.R")
+
+orders <- list(
+  list(drink = "latte", size = "large", milk = "oat", shots = 2),
+  list(drink = "flat white", size = "medium", milk = "dairy", shots = 1),
+  list(drink = "tea", size = "small", milk = "dairy", shots = 1)
+)
+
+cat(receipt(orders, student_discount = TRUE), sep = "\n")
