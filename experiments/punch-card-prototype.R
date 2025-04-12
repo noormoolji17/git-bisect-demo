9153d599d00fb1f45ae494431b877591ed243325
Michael <michael.lydeamore@monash.edu>
2025-04-12 04:46:05 +0000

    Remove unused punch card prototype

--- experiments/punch-card-prototype.R
+++
@@ -1,7 +0,0 @@
-stamp_message <- function(stamps) {
-  remaining <- max(10 - stamps, 0)
-  if (remaining == 0) {
-    return("Free upgrade unlocked")
-  }
-  paste(remaining, "stamps until the next reward")
-}
