e40cab52a9df6220a0c296c265af09ea82ab5050
Rob <rob.hyndman@monash.edu>
2025-06-01 04:30:26 +0000

    Drop scratch comparison script

--- scratch/compare-student-discounts.R
+++
@@ -1,9 +0,0 @@
-source("coffee.R")
-
-sample_order <- list(
-  list(drink = "mocha", size = "medium", milk = "soy", shots = 1),
-  list(drink = "tea", size = "small", milk = "dairy", shots = 1)
-)
-
-print(order_total(sample_order))
-print(order_total(sample_order, student_discount = TRUE))
